bb12b0d94c3490975645a10ff606355682ab335c
DAVID-PC\hinor <hinorid@gmail.com>
2021-07-08 01:59:05 +0900

    젠장 런타임 컨버팅은 안되는구먼
    (cherry picked from commit c20d0da9ec88c08552fc6c46571f958d7328800e)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 ﻿#include "gtest/gtest.h"
 #include <functional>
+#include <Windows.h>
 using namespace std;
 
 typedef function<int(int, int)> Function;		//함수 시그니처는 int(int, int) 다
@@ -27,13 +28,13 @@ auto divi = [](int x, int y) -> int
 		return 0;
 };
 
-TEST(기본기, 단위테스트모듈사용해보기)
+TEST(Basic, UnitTestModuleUsage)
 {
 	EXPECT_EQ(10, 10);		//일치하므로 성공
 	EXPECT_EQ(20, 10);		//불일치 하므로 실패
 }
 
-TEST(기본기, 함수시그니처연습해보기ㅎㅅㅎ) {
+TEST(Basic, FunctionSignatureCompatibility) {
 	Function fShape;
 
 	fShape = add;
